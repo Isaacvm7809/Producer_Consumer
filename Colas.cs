c915861ab31d0a070742d64332cd8a5b9c41bb46
Isaac M Vasquez <isaacvm7809@gmail.com>
2024-06-20 19:07:55 -0600

    Add project files.

---
+++ Colas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadCase
+{
+    internal class Colas
+    {
+
+        //array of consumer threads
+        public List<Thread> consumers = [];
+        
+        //Task queue
+        public Queue<Action> tasks = new();
+        
+        //Sync object for locking the task queue
+        public readonly object queuelock = new();
+        
+        //This wait handle notifies consumers of a new task
+        public EventWaitHandle newTaskAvailable = new AutoResetEvent (false);
+        
+        // The sync object for locking the console color
+        private readonly object consoleLock = new();
+
+        //Enqueue a new task
+        public void EnqueueTask(Action task) 
+        {
+            lock (queuelock) 
+            {
+                this.tasks.Enqueue(task);
+            }
+            newTaskAvailable.Set(); 
+        }
+
+        //Thread work for consumers
+        public void DoWork(ConsoleColor color) 
+        {
+            while (true)
+            {
+                Action task = null; 
+                lock (queuelock)
+                {
+                    if (tasks.Count > 0)
+                    {
+                        task = tasks.Dequeue();
+                    }
+                }
+                if (task != null)
+                {
+                    lock (consoleLock)
+                    {
+                        Console.ForegroundColor = color;    
+                    }
+                    //execute task
+                    task();
+                }
+                else
+                    newTaskAvailable.WaitOne(); 
+            }
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using System.Threading;
+using ThreadCase;
+
+class MainClass 
+{
+    private const int iterator = 1000;
+    public static void DoWork() 
+    {
+        for (int i = 0; i < iterator; i++)
+        {
+            Console.Write($"B{i} ", i);
+        }
+    }
+    public static void Main()
+    {
+        //Set up 3 consumers
+        Colas cola = new ();
+        cola.consumers.Add(new Thread(() => { cola.DoWork(ConsoleColor.Red); }   ));
+        cola.consumers.Add(new Thread(() => { cola.DoWork(ConsoleColor.Green); }  ));
+        cola.consumers.Add(new Thread(() => { cola.DoWork(ConsoleColor.Blue); }  ));
+
+        cola.consumers.ForEach((t) => { t.Start(); });
+        while (true) 
+        {
+            //Add a new task
+            Random r = new ();
+            cola.EnqueueTask( ()=> {                 Console.Write (r.Next(10));             });
+            //Simulate workload
+            Thread.Sleep( r.Next(1000) );
+        }
+        
+
+
+        //Thread hilo = new Thread(new ThreadStart(DoWork));
+        //hilo.Start();   
+
+        //Thread hilo = new Thread( () =>   DoWork() );
+        //hilo.Start();
+        //for (int i = 0; i < 9; i++) 
+        //{
+        //    Thread hilo = new Thread(new ThreadStart(DoWork));
+        //    hilo.Name = "Hilo" + i.ToString();
+        //    Console.Write (hilo.Name);    
+        //    hilo.Start();
+        //}
+
+
+
+
+        //for (int i = 0; i < iterator; i++)
+        //{
+        //    Console.Write($"A  ");
+        //}
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
